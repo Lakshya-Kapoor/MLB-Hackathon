cd9dee259a9b53c4fdc9f3553ea58f06e7539d8e
Lakshya-Kapoor <Lakshya.Kapoor@iiitb.ac.in>
2025-01-17 21:51:21 +0530

    Implement database initialization and user model setup

---
+++ backend/models/user.py
@@ -0,0 +1,12 @@
+from beanie import Document
+from pydantic import EmailStr
+
+from typing import List
+
+class User(Document):
+    username: str
+    email: str
+    password: str
+    player_ids: List[int] = []
+    team_ids: List[int] = []
+
